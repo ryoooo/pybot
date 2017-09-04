e8c5afd9aba6ca98e0cfee3136affdf6eb9b486d
root <root@DESKTOP-CJISP97.localdomain>
2017-09-04 17:12:34 +0900

    use text file

--- pybot.py
+++ pybot.py
@@ -1,8 +1,14 @@
-bot_dict = {
-	'こんにちは': 'コンニチハ',
-	'ありがとう': 'ドウイタシマシテ',
-	'さようなら': 'サヨウナラ',
-	}
+command_file = open('pybot.txt', encoding='utf-8')
+raw_data = command_file.read()
+command_file.close()
+lines = raw_data.splitlines()
+
+bot_dict = {}
+for line in lines:
+	word_list = line.split(',')
+	key = word_list[0]
+	response = word_list[1]
+	bot_dict[key] = response
 
 while True:
 	command = input('pybot>')
