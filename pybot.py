84d32f5fe3bd72e3039068bb3db1698ebe759e6c
root <root@DESKTOP-CJISP97.localdomain>
2017-09-04 16:58:29 +0900

    dict

--- pybot.py
+++ pybot.py
@@ -1,5 +1,20 @@
+bot_dict = {
+	'こんにちは': 'コンニチハ'
+	'ありがとう': 'ドウイタシマシテ'
+	'さようなら': 'サヨウナラ'
+	}
+
 while True:
 	command = input('pybot>')
-	print(command)
-	if 'bye' in command:
+	response = ''
+	for key in bot_dict:
+		if message in command:
+			response = bot_dict[message]
+			break
+
+	if not response:
+		response = 'ナニヲイッテルノカワカラナイ'
+	print(response)	
+
+	if 'さようなら' in command:
 		break
