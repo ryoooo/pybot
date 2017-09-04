5e669d8e64d80de75104e99833e28314fc03652e
root <root@DESKTOP-CJISP97.localdomain>
2017-09-04 17:25:30 +0900

    平成判断機能実装

--- pybot.py
+++ pybot.py
@@ -17,10 +17,18 @@ while True:
 		if key in command:
 			response = bot_dict[key]
 			break
+	if '平成' in command:
+		heisei, year_str = command.split(' ')
+		year = int(year_str)
+		if year >= 1989:
+			heisei_year = year - 1988
+			response = '西暦{}年ハ、平成{}年デス'.format(year, heisei_year)
+		else:
+			response = '西暦{}年ハ、平成デハアリマセン'.format(year)
 
 	if not response:
 		response = 'ナニヲイッテルノカワカラナイ'
-	print(response)	
+	print(response)
 
 	if 'さようなら' in command:
 		break
