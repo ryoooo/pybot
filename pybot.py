268fcbec07d017a9a1e5ac58c771e44a42f390a9
root <root@DESKTOP-CJISP97.localdomain>
2017-09-09 20:25:00 +0900

    例外処理の追加

--- pybot.py
+++ pybot.py
@@ -12,12 +12,15 @@ def len_command(command):
 
 def heisei_command(command):
     heisei, year_str = command.split()
-    year = int(year_str)
-    if year >= 1989:
-        heisei_year = year - 1988
-        response = '西暦{}年ハ、平成{}年デス'.format(year, heisei_year)
-    else:
-        response = '西暦{}年ハ、平成デハアリマセン'.format(year)
+    if year_str.isdigit():
+        year = int(year_str)
+        if year >= 1989:
+            heisei_year = year - 1988
+            response = '西暦{}年ハ、平成{}年デス'.format(year, heisei_year)
+        else:
+            response = '西暦{}年ハ、平成デハアリマセン'.format(year)
+    else: 
+        response = '数値を指定シテクダサイ'
     return response
 
 
@@ -34,33 +37,38 @@ for line in lines:
     bot_dict[key] = response
 
 while True:
-    command = input('pybot> ')
-    response = ""
-    for key in bot_dict:
-        if key in command:
-            response = bot_dict[key]
+    try:
+        command = input('pybot> ')
+        response = ""
+        for key in bot_dict:
+            if key in command:
+                response = bot_dict[key]
+                break
+
+        if '平成' in command:
+            response = heisei_command(command)
+        if '長さ' in command:
+            response = len_command(command)
+        if '干支' in command:
+            response = eto_command(command)
+        if '選ぶ' in command:
+            response = choice_command(command)
+        if 'さいころ' in command:
+            response = dice_command()
+        if '今日' in command:
+            response = today_command()
+        if '現在' in command:
+            response = now_command()
+        if '曜日' in command:
+            response = weekday_command(command)
+
+        if not response:
+            response = '何ヲ言ッテルカ、ワカラナイ'
+        print(response)
+
+        if 'さようなら' in command:
             break
-
-    if '平成' in command:
-        response = heisei_command(command)
-    if '長さ' in command:
-        response = len_command(command)
-    if '干支' in command:
-        response = eto_command(command)
-    if '選ぶ' in command:
-        response = choice_command(command)
-    if 'さいころ' in command:
-        response = dice_command()
-    if '今日' in command:
-        response = today_command()
-    if '現在' in command:
-        response = now_command()
-    if '曜日' in command:
-        response = weekday_command(command)
-
-    if not response:
-        response = '何ヲ言ッテルカ、ワカラナイ'
-    print(response)
-
-    if 'さようなら' in command:
-        break
+    except Exception as e:
+        print('予期セヌエラーが発生シマシタ')
+        print('* 種類', type(e))
+        print('* 内容', e)
--- pybot_datetime.py
+++ pybot_datetime.py
@@ -14,14 +14,19 @@ def now_command():
 
 
 def weekday_command(command):
-    data = command.split()
-    year = int(data[1])
-    month = int(data[2])
-    day = int(data[3])
-    one_day = date(year, month, day)
+    try:
+        data = command.split()
+        year = int(data[1])
+        month = int(data[2])
+        day = int(data[3])
+        one_day = date(year, month, day)
 
-    weekday_str = '月火水木金土日'
-    weekday = weekday_str[one_day.weekday()]
+        weekday_str = '月火水木金土日'
+        weekday = weekday_str[one_day.weekday()]
 
-    response = '{} ハ {}曜日デス'.format(one_day, weekday)
+        response = '{} ハ {}曜日デス'.format(one_day, weekday)
+    except IndexError:
+        response = '3ツノ値ヲ指定シテクダサイ'
+    except ValueError:
+        response = '正シイ日付ヲ指定シテクダサイ'
     return response
